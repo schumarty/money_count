e52a0d17f512d11fc42027c1cb1e5ba3f1c09f63
Marty Schumacher <schumarty@gmail.com>
2016-07-17 20:20:55 -0500

    Now use a for loop to iterate over currencies to generate sum

--- counter.js
+++ counter.js
@@ -1,8 +1,13 @@
 sumMoney = function() {
-	var total =
-		$("#pennies > .amount").val() * .01
-		+ $("#nickels > .amount").val() * .05
-		+ $("#dimes > .amount").val() * .10
-		+ $("#quarters > .amount").val() * .25;
+	var monies = $(".money");
+	var total = 0;
+	for (var i = 0; i < monies.length; i++) {
+		var element = $(monies[i]);
+		var value = $(element.children('.value')).val();
+		var amount = $(element.children('.amount')).val();
+		total += value * amount;
+	}
+		
 	$("#totalMoney").html("$" + total.toFixed(2));
 }
+
